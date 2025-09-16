a8807dfa3276726bd3dfedae1b32c669f98b43ec
li-kravt <215831814+li-kravt@users.noreply.github.com>
2025-09-16 18:34:27 +0200

    refactor: TodoList and  add markup

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -6,7 +6,7 @@ export default function TodoList({ tasks }) {
   return (
     <div>
       <ul>
-        {tasks.map((task, index) => <li key={index}>{task}</li>)}
+        {tasks.map((task, index) => <li key={index}> <input type="checkbox"></input> <label>{task}</label> <div><p>date</p></div></li>)}
       </ul>
     </div>
   )
