9f18749e197ddf327c1964a4cbfe367b4991ad28
li-kravt <215831814+li-kravt@users.noreply.github.com>
2025-09-19 11:32:28 +0200

    style: add minimal style for filters

--- src/components/TodoFilters.jsx
+++ src/components/TodoFilters.jsx
@@ -3,18 +3,24 @@ import React, { useState } from "react";
 export default function TodoFilters() {
 
   return (
-    <div>
-      <select>
-        <option>all</option>
-        <option>deadline</option>
-        <option>completed</option>
-      </select>
-      <select>
-        <option>Added date</option>
-        <option>deadline</option>
-        <option>alphabetically</option>
-      </select>
-      <img src="" alt="reverse" />
+    <div className="flex justify-end px-80 gap-10 my-4">
+      <div className="flex flex-row gap-2 items-center justify-center">
+        <label className="font-light">Filter: </label>
+        <select className="flex items-center justify-center bg-white p-1 rounded-md">
+          <option>All</option>
+          <option>deadline</option>
+          <option>completed</option>
+        </select>
+      </div>
+      <div className="flex flex-row gap-2 items-center justify-center">
+        <label className="font-light">Sort: </label>
+        <select className="flex items-center justify-center bg-white p-1 rounded-md">
+          <option>Added date</option>
+          <option>deadline</option>
+          <option>alphabetically</option>
+        </select>
+      </div>
+      <img src="/img/sort.svg" alt="reverse" className="cursor-pointer" />
     </div>
   )
 }
