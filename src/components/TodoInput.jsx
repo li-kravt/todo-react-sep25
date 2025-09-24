f401b01e09864c37d1c9e6622fcd0d2d007f769f
li-kravt <215831814+li-kravt@users.noreply.github.com>
2025-09-24 16:02:59 +0200

    feat: add add task onKeyDown

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -23,7 +23,9 @@ export default function TodoInput({ tasks, setTasks }) {
 
   return (
     <div className="flex flex-row h-16 w-220 p-4 items-center justify-between bg-white rounded-xl">
-      <input className="flex w-180" type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Add new ..." />
+      <input className="flex w-180" type="text" value={text} onChange={(e) => setText(e.target.value)}
+        onKeyDown={(e) => e.key === "Enter" && handleClick()}
+        placeholder="Add new ..." />
       <div className="flex justify-center items-center gap-4">
         <input type="date" className="w-h h-6 cursor-pointer"
           src="img/calendar.svg"
@@ -33,7 +35,8 @@ export default function TodoInput({ tasks, setTasks }) {
         />
         {/* добавила календарь в инпуте если что, дальше над ним работаю */}
         <button className="flex items-center justify-center p-3 bg-blue-600 text-white leading-none rounded-sm cursor-pointer"
-          onClick={handleClick}>Add</button>
+          onClick={handleClick}
+        >Add</button>
       </div>
     </div>
   )
